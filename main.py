3c08eb96f4dce8904b1c48c03dfc2c403665dfad
Andrei Amorim <76476855+andrei406@users.noreply.github.com>
2021-03-20 16:32:57 -0300

    Chão e sol adicionado

--- main.py
+++ main.py
@@ -25,12 +25,19 @@ fase1.set_volume(1)
 select = pygame.mixer.Sound(os.path.join(diretorio_m, 'selec.wav'))
 select.set_volume(1)
 
+cor_tela = (0, 255, 255)
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Ci Journey')
+a = 2
+
+"""
+Imagem de fundo retirada
 
 imagem_fundo = pygame.image.load(os.path.join(diretorio_i, 'cenario-basico.png')).convert()
 imagem_fundo = pygame.transform.scale(imagem_fundo, (largura,altura))
+"""
 ci_shet = pygame.image.load(os.path.join(diretorio_i, 'ci-princes-sprites.png')).convert_alpha()
+cenario_shet = pygame.image.load(os.path.join(diretorio_i, 'cenario-sprites.png'))
 class Ci(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -40,33 +47,51 @@ class Ci(pygame.sprite.Sprite):
             img = ci_shet.subsurface((i * 32, 0), (32, 32))
             img = pygame.transform.scale(img, (32* 2, 32 * 2))
             self.ci_img.append(img)
-    
         self.index_lista = 0
         self.image = self.ci_img[self.index_lista]
         self.rect = self.image.get_rect()
-        self.rect.center = (50, altura-55)
+        self.rect.center = (50, altura-70)
     def vai(self):
         self.siga = True
     def update(self):
- 
         if self.index_lista > 1:
             self.index_lista = 0
         self.index_lista += .10
         self.image = self.ci_img[int(self.index_lista)]
-            
+class Chao(pygame.sprite.Sprite):
+    def __init__(self, pos_x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = cenario_shet.subsurface(((32 * 3) - 32, 0), (32, 32))
+        self.image = pygame.transform.scale(self.image, (32*a, 32*a))
+        self.rect = self.image.get_rect()
+        self.rect.y = altura - 60
+        self.rect.x = pos_x * 64
+class Sol(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = cenario_shet.subsurface((32, 32), (32, 32))
+        self.image = pygame.transform.scale(self.image, (32*3, 32*3))
+        self.rect = self.image.get_rect()
+        self.rect.x = largura - 90
+        self.rect.y = altura - 450
+#class Arvore(pygame.sprite.Sprite):
+#class Arbusto(pygame.sprite.Sprite):
 
-
-#class Cenario(pgame.sprite.Sprite):
-    
-#
 start = False
 todasSprites = pygame.sprite.Group()
+
+for c in range(largura*2//64):
+    chao = Chao(c)
+    todasSprites.add(chao)
+sol = Sol()
+todasSprites.add(sol)
 ci = Ci()
 todasSprites.add(ci)
+
 relogio = pygame.time.Clock()
 while True:
     relogio.tick(30)
-    tela.blit(imagem_fundo, (0,0))
+    tela.fill(cor_tela)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
